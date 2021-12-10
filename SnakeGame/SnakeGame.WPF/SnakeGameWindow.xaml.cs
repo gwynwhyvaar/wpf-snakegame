3d5d7013c740ed96e1796851b066ed37e729608e
Ayotunde Ayoko <ayotunde.ayoko@un.org>
2021-12-10 13:10:47 +0300

    Added Game area

--- SnakeGame/SnakeGame.WPF/SnakeGameWindow.xaml.cs
+++ SnakeGame/SnakeGame.WPF/SnakeGameWindow.xaml.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace SnakeGame.WPF
@@ -19,9 +11,45 @@ namespace SnakeGame.WPF
     /// </summary>
     public partial class SnakeGameWindow : Window
     {
+        const int SnakeSquareSize = 20;
         public SnakeGameWindow()
         {
             InitializeComponent();
+        }
+        private void Window_ContentRendered(object sender, EventArgs e) => DrawGameArea();
+        private void DrawGameArea()
+        {
+            bool doneDrawingBackground = false;
+            int nextX = 0, nextY = 0;
+            int rowCounter = 0;
+            bool nextIsOdd = false;
+
+            while (doneDrawingBackground is false)
+            {
+                var rect = new Rectangle
+                {
+                    Fill = nextIsOdd ? Brushes.White : Brushes.Black,
+                    Width = SnakeSquareSize,
+                    Height = SnakeSquareSize
+                };
+                GameArea.Children.Add(rect);
+                Canvas.SetTop(rect, nextY);
+                Canvas.SetLeft(rect, nextX);
+
+                nextIsOdd = !nextIsOdd;
+                nextX += SnakeSquareSize;
+                if (nextX >= GameArea.ActualWidth)
+                {
+                    nextX = 0;
+                    nextY += SnakeSquareSize;
+                    rowCounter++;
+                    nextIsOdd = rowCounter % 2 != 0;
+                }
+                if (nextY >= GameArea.ActualHeight)
+                {
+                    doneDrawingBackground = true;
+                }
+            }
         }
     }
 }
