ed74e231f19c434938acd642abc7ccf4c37cdf49
Ayotunde Ayoko <ayotunde.ayoko@un.org>
2021-12-10 12:38:31 +0300

    removed main window xmal

--- SnakeGame/SnakeGame.WPF/SnakeGameWindow.xaml.cs
+++ SnakeGame/SnakeGame.WPF/SnakeGameWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SnakeGame.WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SnakeGameWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SnakeGameWindow : Window
     {
-        public MainWindow()
+        public SnakeGameWindow()
         {
             InitializeComponent();
         }
